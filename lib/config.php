ec7df2342c140eac6e9624e661c1a5da88d6284c
Lucien <lucien@lucien.ink>
2018-08-20 23:54:19 +0800

    Delete config.php

--- lib/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-    'dbhost' => 'localhost:3306',
-    'username' => 'root',
-    'password' => 'root',
-    'dbname' => 'pasteme',
-    'website' => 'http://localhost/',
-);
-?>
