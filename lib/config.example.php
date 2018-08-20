67f1c09f5ff0beaa9ba133302f867472f64e1bb3
lucienshui <18952904@qq.com>
2018-08-20 23:52:50 +0800

    add config.example.php

---
+++ lib/config.example.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'dbhost' => 'localhost:3306',
+    'username' => 'root',
+    'password' => 'root',
+    'dbname' => 'pasteme',
+    'website' => 'http://localhost/',
+);
+?>
