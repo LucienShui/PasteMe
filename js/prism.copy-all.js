6bf491faefe47dc7b6d8e326e9b996da960aaa9b
lucienshui <18952904@qq.com>
2018-08-26 00:12:43 +0800

    adjust btn's change time and the word on btn when success/failed

--- js/prism.copy-all.js
+++ js/prism.copy-all.js
@@ -6,13 +6,13 @@ Prism.plugins.toolbar.registerButton('select-code', function(env) {
     const clipboard = new ClipboardJS('.btn-copy');
     clipboard.on('success', function(item) {
         // alert('复制成功');
-        button.innerHTML = '复制成功';
+        button.innerHTML = '成功';
         item.clearSelection(); // 清除选中内容
-        window.setTimeout(function () {button.innerHTML = '复制'}, 2000);
+        window.setTimeout(function () {button.innerHTML = '复制'}, 1500);
     });
     clipboard.on('error', function() {
-        button.innerHTML = '复制失败';
-        window.setTimeout(function () {button.innerHTML = '复制'}, 2000);
+        button.innerHTML = '失败';
+        window.setTimeout(function () {button.innerHTML = '复制'}, 1500);
     });
     return button;
 });
