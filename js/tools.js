c03751c126e4850e29e939adce4d65f2b19d09ea
lucienshui <18952904@qq.com>
2018-08-25 23:43:15 +0800

    add some js back

---
+++ js/tools.js
@@ -0,0 +1,29 @@
+function enterJudge() {
+    if (window.event.keyCode === 13) {
+        redirect();
+    }
+}
+
+function check(ch) {
+    return ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z') || ('0' <= ch && ch <= '9');
+}
+
+function stringCheck(str) {
+    let len = str.length;
+    if (len > 20) return false;
+    for (let i = 0; i < len; i++) {
+        let buf = str.charAt(i);
+        if (!check(buf)) return false;
+    }
+    return true;
+}
+
+function redirect() {
+    const id = document.getElementById("keyword").value;
+    if (id.length === 0) {
+        alert("索引串不能为空");
+    } else {
+        if (stringCheck(id)) window.location.href = id;
+        else alert("索引串只能由大小写英文字母或数字组成且长度小于20");
+    }
+}
