a5065413d8b94bb593e45c3cb352457ee3f4df3c
lucienshui <18952904@qq.com>
2018-08-26 22:09:26 +0800

    adjusge the style and the javascript

--- js/prism.copy-all.js
+++ js/prism.copy-all.js
@@ -1,18 +1,75 @@
-Prism.plugins.toolbar.registerButton('select-code', function(env) {
-    const button = document.createElement('button');
-    button.innerHTML = '复制';
-    button.className += 'btn-copy';
-    button.setAttribute("data-clipboard-target", 'code');
-    const clipboard = new ClipboardJS('.btn-copy');
-    clipboard.on('success', function(item) {
-        // alert('复制成功');
-        button.innerHTML = '成功';
-        item.clearSelection(); // 清除选中内容
-        window.setTimeout(function () {button.innerHTML = '复制'}, 1500);
+(function(){
+    if (typeof self === 'undefined' || !self.Prism || !self.document) {
+        return;
+    }
+
+    if (!Prism.plugins.toolbar) {
+        console.warn('Copy to Clipboard plugin loaded before Toolbar plugin.');
+
+        return;
+    }
+
+    let ClipboardJS = window.ClipboardJS || undefined;
+
+    if (!ClipboardJS && typeof require === 'function') {
+        ClipboardJS = require('clipboard');
+    }
+
+    let callbacks = [];
+
+    if (!ClipboardJS) {
+        let script = document.createElement('script');
+        let head = document.querySelector('head');
+
+        script.onload = function() {
+            ClipboardJS = window.ClipboardJS;
+
+            if (ClipboardJS) {
+                while (callbacks.length) {
+                    callbacks.pop()();
+                }
+            }
+        };
+
+        script.src = 'https://cdn.bootcss.com/clipboard.js/2.0.1/clipboard.min.js';
+        head.appendChild(script);
+    }
+
+    Prism.plugins.toolbar.registerButton('copy-to-clipboard', function (env) {
+        let linkCopy = document.createElement('a');
+        linkCopy.textContent = '复制';
+
+        if (!ClipboardJS) {
+            callbacks.push(registerClipboard);
+        } else {
+            registerClipboard();
+        }
+
+        return linkCopy;
+
+        function registerClipboard() {
+            let clip = new ClipboardJS(linkCopy, {
+                'text': function () {
+                    return env.code;
+                }
+            });
+
+            clip.on('success', function() {
+                linkCopy.textContent = '复制成功';
+
+                resetText();
+            });
+            clip.on('error', function () {
+                linkCopy.textContent = '复制失败';
+
+                resetText();
+            });
+        }
+
+        function resetText() {
+            setTimeout(function () {
+                linkCopy.textContent = '复制';
+            }, 2000);
+        }
     });
-    clipboard.on('error', function() {
-        button.innerHTML = '失败';
-        window.setTimeout(function () {button.innerHTML = '复制'}, 1500);
-    });
-    return button;
-});
+})();
