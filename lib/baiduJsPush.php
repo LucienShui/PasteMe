9a0917dc0707d16086be35c2c9843bb8be38582e
LucienShui <lucien@lucien.ink>
2018-12-03 22:22:48 +0800

    update baiduJsPush.php

--- lib/baiduJsPush.php
+++ lib/baiduJsPush.php
@@ -12,3 +12,12 @@
     s.parentNode.insertBefore(bp, s);
 })();
 </script>
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?0734706417cf0d48f7a01e017629f9f1";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
