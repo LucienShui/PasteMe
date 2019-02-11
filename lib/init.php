f3d21002cfb6e0994d5f23a26d9c7318efb61b56
LucienShui <lucien@lucien.ink>
2019-02-11 15:47:29 +0800

    replace one tab to four spaces

--- lib/init.php
+++ lib/init.php
@@ -9,26 +9,26 @@ $sqlSet = [
       `name` VARCHAR (107) PRIMARY KEY,
       `id` BIGINT
     )",
-		"INSERT INTO `id` VALUES ('id', 100)",
-		"CREATE TABLE IF NOT EXISTS `temp` (
-		  `key` BIGINT PRIMARY KEY,
-		  `text` TEXT,
-		  `type` VARCHAR (107),
-		  `passwd` VARCHAR (37),
-			`visble` BOOLEAN
-		)",
+        "INSERT INTO `id` VALUES ('id', 100)",
+        "CREATE TABLE IF NOT EXISTS `temp` (
+          `key` BIGINT PRIMARY KEY,
+          `text` TEXT,
+          `type` VARCHAR (107),
+          `passwd` VARCHAR (37),
+            `visble` BOOLEAN
+        )",
 ];
 foreach ($sqlSet as $sql) if (!$connection->query($sql)) echo 'Error: ' . mysqli_error($connection);
 for ($i = 0; $i < 10; $i++) {
-	$sqlSet = [
-		"CREATE TABLE IF NOT EXISTS `perm{$i}` (
-		  `key` BIGINT PRIMARY KEY,
-		  `text` TEXT,
-		  `type` VARCHAR (107),
-		  `passwd` VARCHAR (37),
-			`visble` BOOLEAN
-		)"
-		];
-	foreach ($sqlSet as $sql) if (!$connection->query($sql)) echo 'Error: ' . mysqli_error($connection);
+    $sqlSet = [
+        "CREATE TABLE IF NOT EXISTS `perm{$i}` (
+          `key` BIGINT PRIMARY KEY,
+          `text` TEXT,
+          `type` VARCHAR (107),
+          `passwd` VARCHAR (37),
+            `visble` BOOLEAN
+        )"
+        ];
+    foreach ($sqlSet as $sql) if (!$connection->query($sql)) echo 'Error: ' . mysqli_error($connection);
 }
 echo '<br>如果没有报错的话就代表部署成功啦，快去访问主页看看吧！';
