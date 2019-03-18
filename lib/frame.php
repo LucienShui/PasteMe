3dc80612b1721a5342acfc2251e860ad42983cd0
LucienShui <lucien@lucien.ink>
2019-03-18 12:47:30 +0800

    fix url bug at success page

--- lib/frame.php
+++ lib/frame.php
@@ -77,7 +77,7 @@ function head($color = '', $title = 'PasteMe - 一个不算糟糕的可私有文
 
 function home($seed, $placeholder = '写点什么进来吧', $action = 'lib/submit.php', $visibility = '') {
     global $config;
-    $url = "{$config['protocol']}{$config['domain']}{$config['path']}{{ keyword }}";
+    $base_url = "{$config['protocol']}{$config['domain']}{$config['path']}";
     ?>
             <div class="row">
                 <div class="col-md-2">
@@ -135,8 +135,8 @@ function home($seed, $placeholder = '写点什么进来吧', $action = 'lib/subm
                         </p>
                         <ul>
                             <li>在主页中输入索引</li>
-                            <li>在浏览器中访问：<a id="url" href="<?php echo $url?>"><?php echo $url?></a> <a tabindex="1" id="copy" href="#">复制链接</a></li>
-                            <li><a id="success_qrcode_link" href = 'http://api.lucien.ink/qrcode/?text=<?php echo $url?>&tag=PasteMe - 可能是最low的文本分享平台' target='_blank'>扫描二维码</a></li>
+                            <li>在浏览器中访问：<a id="url" v-bind:href="'<?php echo $base_url?>' + keyword"><?php echo $base_url?>{{ keyword }}</a> <a tabindex="1" id="copy" href="#">复制链接</a></li>
+                            <li><a id="success_qrcode_link" v-bind:href="'http://api.lucien.ink/qrcode/?text=<?php echo $base_url?>' + keyword + '&tag=PasteMe - 可能是最low的文本分享平台'" target="_blank">扫描二维码</a></li>
                         </ul>
                         <p>
                             <a class="btn btn-primary btn-large" tabindex="2" id="success_home_button" onclick="success_home_button()">返回主页</a>
