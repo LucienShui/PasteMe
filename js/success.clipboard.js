93186d94b10f8e6100f52606197a5107035b7a4e
LucienShui <lucien@lucien.ink>
2019-03-21 11:42:51 +0800

    bug fix of clipboard.js

--- js/success.clipboard.js
+++ js/success.clipboard.js
@@ -1,10 +1,9 @@
-let copyLink = document.getElementById('copy');
 const clipboard = new ClipboardJS('#copy');
 clipboard.on('success', function() {
-    copyLink.innerText = '复制成功';
-    window.setTimeout(function () {copyLink.innerText = '复制链接'}, 2000);
+    document.getElementById('copy').innerText = '复制成功';
+    window.setTimeout(function () {document.getElementById('copy').innerText = '复制链接'}, 2000);
 });
 clipboard.on('error', function() {
-    copyLink.innerText = '复制失败';
-    window.setTimeout(function () {copyLink.innerText = '复制链接'}, 2000);
+    document.getElementById('copy').innerText = '复制失败';
+    window.setTimeout(function () {document.getElementById('copy').innerText = '复制链接'}, 2000);
 });
--- lib/frame.php
+++ lib/frame.php
@@ -135,7 +135,7 @@ function home($seed, $placeholder = '写点什么进来吧', $action = 'lib/subm
                         </p>
                         <ul>
                             <li>在主页中输入索引</li>
-                            <li>在浏览器中访问：<a id="url" v-bind:href="'<?php echo $base_url?>' + keyword"><?php echo $base_url?>{{ keyword }}</a> <a tabindex="1" id="copy" href="#" v-bind:data-clipboard-text="'<?php echo $base_url?>' + keyword">复制链接</a></li>
+                            <li>在浏览器中访问：<a id="url" v-bind:href="'<?php echo $base_url?>' + keyword"><?php echo $base_url?>{{ keyword }}</a> <a tabindex="1" id="copy" href="#" base="<?php echo $base_url?>">复制链接</a></li>
                             <li><a id="success_qrcode_link" v-bind:href="'http://api.lucien.ink/qrcode/?text=<?php echo $base_url?>' + keyword + '&tag=PasteMe - 可能是最low的文本分享平台'" target="_blank">扫描二维码</a></li>
                         </ul>
                         <p>
