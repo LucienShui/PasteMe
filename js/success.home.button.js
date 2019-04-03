81f22148e51b498c35e48e2f190572ba0ad58085
LucienShui <lucien@lucien.ink>
2019-04-03 22:52:37 +0800

    js compressed

--- js/success.home.button.js
+++ js/success.home.button.js
@@ -1,15 +1,1 @@
-function success_home_button() {
-    $("#index_form")[0].reset();
-    var elem_form = document.getElementById("index_form");
-    var idx = elem_form.action.indexOf('?');
-    if (idx > -1) {
-        elem_form.action = elem_form.action.substr(0, idx);
-        console.log(elem_form.action);
-    }
-    var form_submit_button = document.getElementById("index_form_button");
-    form_submit_button.innerText = "保存";
-    form_submit_button.disabled = undefined;
-    $("#success_div").fadeOut("fast", function() {
-        $("#home_div").fadeIn("fast");
-    });
-}
+"use strict";function success_home_button(){$("#index_form")[0].reset();var e=document.getElementById("index_form"),t=e.action.indexOf("?");t>-1&&(e.action=e.action.substr(0,t),console.log(e.action));var n=document.getElementById("index_form_button");n.innerText="保存",n.disabled=void 0,$("#success_div").fadeOut("fast",function(){$("#home_div").fadeIn("fast")})}
