eb88b915858c4cc4082439c5b76ce67c2cf5d967
Lucien Shui <lucien@lucien.ink>
2019-07-01 10:47:42 +0800

    add frontend & backend submodule

--- Backend/lib/config.example.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-    'dbhost' => 'localhost:3306', // 数据库服务端地址
-    'username' => 'root', // 数据库用户名
-    'password' => 'root', // 数据库密码
-    'dbname' => 'pasteme', // 数据库名
-    'passwd' => 'CHANGE_THIS', // 管理界面的密码
-);
